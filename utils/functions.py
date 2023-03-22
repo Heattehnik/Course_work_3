22be6f6115f09f1a124e8b35874d1e08c1d63fad
Heattehnik <thegadjka@gmail.com>
2023-03-22 20:56:43 +0500

    get_executed()

--- utils/functions.py
+++ utils/functions.py
@@ -1,8 +1,16 @@
 import json
 
 
-def load_operations(path: str) -> dict:
+def load_operations(path: str) -> list:
     with open(path, "rb", encoding="UTF-8") as file:
-        dict_ = json.load(file)
-    return dict_
+        list_ = json.load(file)
+    return list_
+
+
+def get_executed(operations: list) -> list:
+    result_list = []
+    for item in operations:
+        if item["state"] == "EXECUTED":
+            result_list.append(item)
+    return result_list
 
